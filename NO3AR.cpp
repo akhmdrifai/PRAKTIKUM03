ddae5f7fdaa7b8d5ca66c895906a0703ab821e54
akhmdrifai <32448266+akhmdrifai@users.noreply.github.com>
2017-11-06 23:23:43 -0800

    membuat rancangan gaji karyawan

--- NO3AR.cpp
+++ NO3AR.cpp
@@ -5,4 +5,21 @@ int main(){
 	int gp,jk;
 	int lembur,gt,pajak,gk;
 	
+	cout<<"Perhitungan Gaji Karyawan"<<endl;
+	cout<<"Masukan Gaji Pokok Anda : Rp. ";
+	cin >>gp;
+	cout <<"Masukan Jam Kerja Anda : ";
+	cin >>jk;
+	
+	lembur=jk-40;
+	gt=(lembur*1.5+40)*gp;
+	pajak=gt*0.15;
+	gk=gt-pajak;
+	
+	cout<<endl;
+	cout<<"Gaji Pokok  : Rp."<<gp<<endl;
+	cout<<"Jam Kerja   : "<<jk<<endl;
+	cout<<"Lembur      : "<<lembur<<endl;
+	cout<<"Gaji Sebelum Dipotong Pajak : Rp."<<gt<<endl;
+	cout<<"Total Gaji Anda Adalah : Rp."<<gk;
 }
