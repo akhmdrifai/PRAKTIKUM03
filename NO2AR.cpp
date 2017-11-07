c2ec946443b465afb2f08bd078d4f10f8e03dd2f
akhmdrifai <32448266+akhmdrifai@users.noreply.github.com>
2017-11-06 23:19:10 -0800

    menghitung luas persegi panjang dan segitiga

--- NO2AR.cpp
+++ NO2AR.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+
 using namespace std;
 
-int main(){
-
+int main (int argc, char** argv){
+	int panjang, lebar, luas, pilihan, alas, tinggi;
+	
+	cout << "Pilihan Hitungan\n";
+	cout << "1.Persegi Panjang\n";
+	cout << "2.Segitiga\n";cin >>pilihan;
+	
+	switch(pilihan){
+		case 1:
+			cout <<"Masukan Panjang =";cin>>panjang;
+			cout <<"Masukan Lebar =";cin>>lebar;
+			luas =panjang * lebar;
+			cout<<"Luas Persegi Panjang="<<luas<<"\n";
+			break;
+			case 2:
+			cout<<"Masukan Panjang Alas Segitiga=";cin>>alas;
+			cout<<"Masukan Tinggi Segitiga=";cin>>tinggi;
+			luas=0.5*alas*tinggi;
+			cout<<"Luas Segitiga ="<<luas<<"\n";
+	}
 }
