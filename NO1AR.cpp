857263138d098feed910e7c1aeb0032cc4d407e1
akhmdrifai <32448266+akhmdrifai@users.noreply.github.com>
2017-11-06 23:02:02 -0800

    input nilai x dan y

--- NO1AR.cpp
+++ NO1AR.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main(){
+	int x,y,z;
+	
+	cout<<"Menghitung Selisih Absolute"<<endl;
+	cout<<"Masukan Nilai X : ";
+	cin>>x;
+	cout<<"Masukan Nilai Y : ";
+	cin>>y;
 	
 }
